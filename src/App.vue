785f0acdf124ddd5caef6728e0bd83db39dc042f
Fantom84x <wernerdejager883@gmail.com>
2024-05-15 19:48:45 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main style="width: 100vw; height: 100vh; overflow-y: auto">
+      <AppSnakeBoard style="overflow-y: auto"></AppSnakeBoard>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AppSnakeBoard from './components/AppSnakeBoard.vue'
+
+export default {
+  components: {
+    AppSnakeBoard,
+  },
+}
+</script>
+
+<style>
+html, body {
+  overflow-y: hidden;
+}
+</style>
---
+++ src/components/AppSnakeBoard.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    class="b1 d-flex flex-column"
+    style="width: 100%; height: 100%"
+  >
+    <div
+      v-for="(row, i) in cellsInfoArr"
+      :key="i + 'A'"
+      class="d-flex"
+    >
+      <div
+        v-for="(cell, k) in row"
+        :key="k + 'A'"
+        :id="cell"
+        class="d-flex"
+      >
+        <div
+          class="b2"
+          style="width: 30px; height: 30px"
+        ></div>
+
+      </div>
+
+    </div>
+
+    <v-btn
+      text="Click Me"
+      @click="myFunction"
+    ></v-btn>
+
+  </div>
+</template>
+
+<script setup>
+import {onMounted, ref} from "vue";
+
+const cellsInfoArr = ref([]);
+
+function myFunction() {
+  console.log(document.getElementById('43').getBoundingClientRect());
+}
+
+onMounted(() => {
+  for (let i = 0; i < 5; i++) {
+    cellsInfoArr.value.push([]);
+    for (let j = 0; j < 5; j++) {
+      cellsInfoArr.value[i].push(`${i}${j}`);
+    }
+  }
+
+  console.log(cellsInfoArr.value);
+
+})
+
+</script>
+
+<style scoped>
+.b1 {
+  border: 1px solid deeppink;
+}
+.b2 {
+  border: 1px solid deepskyblue;
+}
+.b3 {
+  border: 1px solid yellow;
+}
+</style>
